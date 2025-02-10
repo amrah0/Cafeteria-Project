2403a38452cef97c46af1f3c7f1b730cbff0920e
ManarHamdy <mh2157@fayoum.edu.eg>
2025-02-10 21:45:02 +0200

    Home_User_Screen_2_UI

--- views/user/show.vew.php
+++ views/user/show.vew.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Drink Order Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../../styles/style.css" rel="stylesheet">
 
 </head>
 <body>
@@ -114,7 +114,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../js/script.js"></script>
+    <script src="../../scripts/script.js"></script>
     
 </body>
 </html>
