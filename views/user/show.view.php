58861bd81cd9fdf6ca94db6a0a386039bb548cb1
Mohamed Sofy <mediumsofy@gmail.com>
2025-02-13 14:55:48 +0200

    Rename s2 style.css -> homepage_s2.css to prevent conflict

--- views/user/show.view.php
+++ views/user/show.view.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Drink Order Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../../styles/style.css" rel="stylesheet">
+    <link href="../../styles/homepage_s2.css" rel="stylesheet">
 
 </head>
 <body>
