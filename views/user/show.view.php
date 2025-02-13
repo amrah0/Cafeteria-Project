eb4428736bb7dac8dc3f430b82e0c737cd9bd77a
Mohamed Sofy <mediumsofy@gmail.com>
2025-02-13 14:57:40 +0200

    Rename s2 script.js -> homepage_s2.js to prevent conflict

--- views/user/show.view.php
+++ views/user/show.view.php
@@ -114,7 +114,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../../scripts/script.js"></script>
+    <script src="../../scripts/homepage_s2.js"></script>
     
 </body>
 </html>
