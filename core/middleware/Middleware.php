e49a78a235beb1b0ba44ca783b6f90ffc66b0a8a
Mohamed Sofy <mediumsofy@gmail.com>
2025-02-15 01:02:27 +0200

    Add core/middleware/Middleware.php class

---
+++ core/middleware/Middleware.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace core\middleware;
+
+class Middleware
+{
+    public const Map=[
+      'guest' => Guest::class,
+      'auth' => Authenticated::class
+    ];
+
+    public static function resolve($key)
+    {
+        if(!$key) {
+          return;
+        }
+
+        $middleware = static::Map[$key] ?? false;
+
+        if(!$middleware){
+          throw new \Exception("No matching middleware found for key '{$key}'.");
+        }
+
+        (new $middleware)->handle();
+    }
+    
+}
