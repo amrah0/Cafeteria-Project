a16a6da425cda3a15e975e5f8ea11db2e7a70eb5
Mohamed Sofy <mediumsofy@gmail.com>
2025-02-20 16:17:30 +0200

    Add admin/catalog and admin/users routes

--- routes.php
+++ routes.php
@@ -33,3 +33,14 @@ $router->get('/admin/checks', 'admin/checks/index.php');
 
 // screen-8 route
 $router->post('/admin/products/create', '/admin/product/create.php');
+
+
+// Add authorization rules to each route {admin, user}
+$router->get('/admin/catalog', '/admin/catalog/index.php');
+$router->post('/admin/catalog', '/admin/catalog/store.php');
+
+$router->get('/admin/users/edit', '/admin/users/edit.php');
+$router->post('/admin/users/delete', '/admin/users/destroy.php');
+
+$router->get('/admin/users', '/admin/users/index.php');
+$router->post('/admin/users/edit', '/admin/users/update.php');
