557696b696e500f27bd288a9e3b923dffaa3f140
amrah0 <amroa333@gmail.com>
2025-02-20 05:04:28 +0200

    adding route

--- routes.php
+++ routes.php
@@ -19,3 +19,16 @@ $router->get('/admin/catalog', '/admin/catalog/index.php');
 
 
 $router->post('/admin/products/create', '/admin/product/create.php');
+
+
+
+
+
+
+
+
+
+
+
+// screen 7 route
+$router->post('/admin/users/create', '/admin/users/create.php');
